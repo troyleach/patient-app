2ac757869ca1f32cd02a71dac31a99e2649c5b20
Troy Leach <troyleach29@gmail.com>
2015-04-19 16:40:42 -0500

    change the app.js back to default

--- app/assets/javascripts/app.js
+++ app/assets/javascripts/app.js
@@ -1,6 +1,6 @@
 (function() {
   "use strict";
  
-  angular.module("app", ["ngCsv", "ui.bootstrap", "ngResource", "templates", "angularFileUpload", "ngSanitize", "ngCookies", "textAngular", "djds4rce.angular-socialshare"]);
+  angular.module("app", []);
  
 }());
