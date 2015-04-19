2442c5b0a2d3331156a50e61951bf3628a903932
Troy Leach <troyleach29@gmail.com>
2015-04-19 16:24:18 -0500

    modified the app.js file

--- app/assets/javascripts/app.js
+++ app/assets/javascripts/app.js
@@ -1,6 +1,6 @@
 (function() {
   "use strict";
  
-  angular.module("app", []);
+  angular.module("app", ["ui.router", "ngCsv", "ui.bootstrap", "ngResource", "templates", "angularFileUpload", "ngSanitize", "ngCookies", "textAngular", "djds4rce.angular-socialshare"]);
  
 }());
